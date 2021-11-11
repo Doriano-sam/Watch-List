7dc35ba8d134a4140653bbf69b6b8e38c216d555
Dorian Sammut <dorian.sammut@gmail.com>
2021-11-11 15:55:11 +0100

    first step

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,5 +1,7 @@
 class Bookmark < ApplicationRecord
   belongs_to :movie
   belongs_to :list
-  validates :comment, length: { in: 6..20 }
+
+  validates :movie_id, uniqueness: {scope: :list_id}
+  validates :comment, length: { minimum: 6 }
 end
--- db/migrate/20211111143559_create_bookmarks.rb
+++ db/migrate/20211111143559_create_bookmarks.rb
@@ -1,8 +1,9 @@
 class CreateBookmarks < ActiveRecord::Migration[6.0]
   def change
     create_table :bookmarks do |t|
-      t.movie_id :references
-      t.list_id :references
+      t.text :comment
+      t.references :movie, null: false, foreign_key: true
+      t.references :list, null: false, foreign_key: true
 
       t.timestamps
     end
